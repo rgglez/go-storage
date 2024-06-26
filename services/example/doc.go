c45437e5d939280d2cba3c74121defe9a55ea048
Rodolfo Gonzalez <rodolfo.gonzalez+github@gmail.com>
2024-06-25 20:16:43 -0600

    services-oss-v3.0.0

--- services/example/doc.go
+++
@@ -1,6 +0,0 @@
-/*
-Package example provided support for local file system.
-*/
-package example
-
-//go:generate go run -tags tools ./internal/cmd
--- services/us3/doc.go
+++
@@ -1,6 +0,0 @@
-/*
-Package us3 provided support for Ucloud(https://docs.ucloud.cn).
-*/
-package us3
-
-//go:generate go run ./internal/cmd
