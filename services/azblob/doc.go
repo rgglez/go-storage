8ef997e2d7be45758ec86386888715d99b077be1
Rodolfo Gonzalez <rodolfo.gonzalez+github@gmail.com>
2024-06-25 19:56:00 -0600

    endpoint-v1.2.1

--- services/azblob/doc.go
+++
@@ -1,6 +0,0 @@
-/*
-Package azblob provided support for Azure Storage containers and blobs objects (https://docs.microsoft.com/en-us/azure/storage/blobs/storage-blobs-introduction)
-*/
-package azblob
-
-//go:generate go run -tags tools ./internal/cmd
--- services/ocios/doc.go
+++
@@ -1,6 +0,0 @@
-/*
-Package ocios provided support for Oracle Object Storage(https://www.oracle.com/cloud/storage/object-storage/).
-*/
-package ocios
-
-//go:generate go run ./internal/cmd
