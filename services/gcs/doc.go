e96457394d88a3cec997baba96dc03375775555d
Rodolfo Gonzalez <rodolfo.gonzalez+github@gmail.com>
2024-07-10 19:18:15 -0600

    services-s3-v3.0.3

--- services/gcs/doc.go
+++
@@ -1,6 +0,0 @@
-/*
-Package gcs provided support for Google Cloud Storage (https://cloud.google.com/storage/)
-*/
-package gcs
-
-//go:generate go run ./internal/cmd
--- services/obs/doc.go
+++
@@ -1,6 +0,0 @@
-/*
-Package obs provided support for the Huawei Object Storage Service(https://www.huaweicloud.com/intl/en-us/product/obs.html).
-*/
-package obs
-
-//go:generate go run ./internal/cmd
